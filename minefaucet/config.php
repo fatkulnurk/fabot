522ef8e488bdda8908d54a5c25a4436f5e398758
Fatkul Nur Koirudin <fatkulnurk@gmail.com>
2021-07-03 21:16:34 +0700

    Update config.php

--- minefaucet/config.php
+++ minefaucet/config.php
@@ -5,6 +5,9 @@ return [
     'wallets' => [
         'DTCHSoiC9GF2ivKtnbY3jGG6mVS5jgiRQt',
         'DJKmyk3y3xKg8CbbRQ4yrQjj9CrKCXZJcu',
-        'DQgEWeRgQjzMjgq92VWgNJuhJA5uPUm3c9'
+        'DQgEWeRgQjzMjgq92VWgNJuhJA5uPUm3c9',
+'D7xZqoSLbDWxjTY96RLu2EHjH1Mc5RR5Dn', 
+'D8znZZf4W4omzwYvdbYwEmgmZyCDQzs8tY', 
+'DQNFjs25EKGzy7ACgFyECctwhYyAWjxKrf'
     ]
 ];
