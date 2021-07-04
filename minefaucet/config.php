80c27b46e7e59e56e655cc70a25c3311c9470ac4
Fatkul Nur Koirudin <fatkulnurk@gmail.com>
2021-07-04 18:23:05 +0700

    Update config.php

--- minefaucet/config.php
+++ minefaucet/config.php
@@ -6,8 +6,20 @@ return [
         'DTCHSoiC9GF2ivKtnbY3jGG6mVS5jgiRQt',
         'DJKmyk3y3xKg8CbbRQ4yrQjj9CrKCXZJcu',
         'DQgEWeRgQjzMjgq92VWgNJuhJA5uPUm3c9',
-'D7xZqoSLbDWxjTY96RLu2EHjH1Mc5RR5Dn', 
-'D8znZZf4W4omzwYvdbYwEmgmZyCDQzs8tY', 
-'DQNFjs25EKGzy7ACgFyECctwhYyAWjxKrf'
+        'D7xZqoSLbDWxjTY96RLu2EHjH1Mc5RR5Dn', 
+        'D8znZZf4W4omzwYvdbYwEmgmZyCDQzs8tY', 
+        'DQNFjs25EKGzy7ACgFyECctwhYyAWjxKrf',
+        'DKMJb9ZPXjvu3frxkBVKPn9CpTi9zFXMpa',
+        'D8XmgEjFUjhiXcBfAxUWMvUPhf8hs5ZSe7',
+        'DS98FMUHtdxRPrpJb2Swi5JrDSSkENdyx2',
+        'DCpN9SryMe6FVBFYyGR8wCcm7XQjdGr5dQ',
+        'DFLkAXZZtLjZKvupEp6qN6adg2BYggvbUb',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
     ]
 ];
