2d1ce2609489de6f59ffcfb8dfab06ce8d680bc4
Fatkul Nur Koirudin <fatkulnurk@gmail.com>
2021-07-05 18:14:39 +0700

    Update config.php

--- minefaucet/config.php
+++ minefaucet/config.php
@@ -12,7 +12,7 @@ return [
         'DKMJb9ZPXjvu3frxkBVKPn9CpTi9zFXMpa',
         'D8XmgEjFUjhiXcBfAxUWMvUPhf8hs5ZSe7',
         'DS98FMUHtdxRPrpJb2Swi5JrDSSkENdyx2',
-        'DCpN9SryMe6FVBFYyGR8wCcm7XQjdGr5dQ',
+        'DCpN9SryMe6FVBFYyGR8wCcm7XQjdGr5dQ', // 10
         'DFLkAXZZtLjZKvupEp6qN6adg2BYggvbUb',
         'DSEyg7st4J5RgF9e7XUyLgQuJcHeP9AtSR',
         'DDjVcm8PSUniaYYkFXppFU3PQzvn7ynefp',
@@ -22,10 +22,17 @@ return [
         'DKCLbmLnRFpcRAQHQDVht4oaGJhj1gQxEV',
         'DCr1cuk4XASe18NkeTHEVKP52KYBMhEkF4',
         'DBMwZJmBEDr1EaiVgr69xAdmaKpN6tdBes',
-        'DPp4319sL7suSn6irnPs4YHuvDJSeDmwJF', // wawan@dibumi.com
+        'DPp4319sL7suSn6irnPs4YHuvDJSeDmwJF', // wawan@dibumi.com // 20
         'DPFxLwZ37erhchDFQChhRppmUwsh6ESx2F',//  dino@dibumi.com
         'DAS4uqJSuG3LvkUGYgHsToMkaBaYTNyLtn',
         'DAGmDfZoXEnSrZ4HieuTdDcnsYM3deUuej',
+        'DSXdCJRcgrxK3zHCNDNNLDc8WhECztA7f8',
+        'DGM5MxZSceXdoEZZnArHq2oz1bFFk45C63',
+        'DJFxpBTZHroWJKGTVVD3xjJAR7fDxKGGE7',
+        'DDUoeJSZm6RSkgtoigJzm9kLcniXpupECZ',
+        'DFiqWnMVy3qSem8JQGxvotnJyxMgAiENow',
+        'DGFEQdA5oimNHaQvfnNoj6mQYYr1LdKESH',
+        'D6UKrfepumbzuEubvkdNB8iUTiKjrmvfFa', //30
         '',
         '',
         '',
